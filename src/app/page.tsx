39c4e0bd83ef16400e8ee66b553ec3e2eeae770b
Laercio Rios <contact@laerciorios.com>
2024-09-25 08:50:25 -0300

    feat(home): add experience in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,25 @@
 "use client";
 import "./i18n";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
+import { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import ArrowDownIcon from "./assets/icons/arrow-down.svg";
-import ArrowRight from "./assets/icons/arrow-right-alt.svg";
+// import ArrowRight from "./assets/icons/arrow-right-alt.svg";
 import EmailIcon from "./assets/icons/email-logo.svg";
 import GithubIcon from "./assets/icons/github-logo.svg";
 import LinkedinIcon from "./assets/icons/linkedin-logo.svg";
 import Logo from "./assets/logo.svg";
 import profileImg from "./assets/profile.png";
+import ExperienceCard from "./components/ExperienceCard";
 import Footer from "./components/Footer";
 import FormationCard from "./components/FormationCard";
 import Header from "./components/Header";
 import PresentationCard from "./components/PresentationCard";
 import ProjectCard from "./components/ProjectCard";
 import Typography from "./components/Typography";
+import { experiences } from "./data/experiences";
 import { formations } from "./data/formations";
 import { presentations } from "./data/presentations";
 import { projects } from "./data/projects";
@@ -25,8 +28,34 @@ import { GITHUB_LINK, EMAIL_LINK, LINKEDIN_LINK } from "./utils/contrants";
 
 export default function Home() {
   const { t } = useTranslation();
+  const { innerWidth: windowWidth } = window;
 
   const projectsHighlighted = projects.filter((project) => project.highlight);
+  const [experiencesWithActive, setExperiencesWithActive] = useState(
+    experiences.map((experience, index) => ({
+      ...experience,
+      active: index === 0 && windowWidth > 1024,
+    })),
+  );
+
+  const activeExperienceSummary = useMemo(() => {
+    const activeExperience = experiencesWithActive.find(
+      (experience) => experience.active,
+    );
+    return activeExperience
+      ? t(`data.experiences.${activeExperience.title}.description.label`)
+      : "";
+  }, [experiencesWithActive, t]);
+
+  function handleActiveExperienceChange(title: string) {
+    const updatedExperiences = experiencesWithActive.map((experience) => ({
+      ...experience,
+      active:
+        experience.title === title &&
+        (!experience.active || windowWidth > 1024),
+    }));
+    setExperiencesWithActive(updatedExperiences);
+  }
 
   return (
     <main className={styles.home}>
@@ -36,7 +65,7 @@ export default function Home() {
           <Logo className={styles["home-intro_section-logo"]} />
           <div className={styles["home-intro_section-title"]}>
             <Typography variant="h1">Laercio Rios</Typography>
-            <Typography variant="h2">{t("profile.role")}</Typography>
+            <Typography variant="h2">{t("profile.position")}</Typography>
           </div>
           <ul className={styles["home-intro_section-networks"]}>
             <li>
@@ -80,12 +109,12 @@ export default function Home() {
             <Typography variant="h3">
               {t("pages.home.projects.title.label")}
             </Typography>
-            <Link href="/projects">
+            {/* <Link href="/projects">
               <Typography variant="body1">
                 {t("pages.home.projects.seeMore.label")}
               </Typography>
               <ArrowRight />
-            </Link>
+            </Link> */}
           </div>
           <ul className={styles["projects-list"]}>
             {projectsHighlighted.map((project) => (
@@ -106,12 +135,55 @@ export default function Home() {
             <Typography variant="h3">
               {t("pages.home.experience.title.label")}
             </Typography>
-            <Link href="/">
+            {/* <Link href="/">
               <Typography variant="body1">
                 {t("pages.home.experience.seeMore.label")}
               </Typography>
               <ArrowRight />
-            </Link>
+            </Link> */}
+          </div>
+          <ul className={styles["experience-nested_list"]}>
+            {experiencesWithActive.map((experience) => (
+              <li key={experience.title}>
+                <ExperienceCard
+                  title={experience.title}
+                  company={experience.company}
+                  startDate={experience.startDate}
+                  endDate={experience.endDate}
+                  active={experience.active}
+                  hasOpenIcon
+                  onClick={() => handleActiveExperienceChange(experience.title)}
+                />
+                {experience.active && (
+                  <Typography variant="body2">
+                    {t(
+                      `data.experiences.${experience.title}.description.label`,
+                    )}
+                  </Typography>
+                )}
+              </li>
+            ))}
+          </ul>
+          <div className={styles["experience-content"]}>
+            <div className={styles["experience-summary"]}>
+              <Typography variant="h4">
+                {t("pages.home.experience.summary.label")}
+              </Typography>
+              <Typography variant="body2">{activeExperienceSummary}</Typography>
+            </div>
+            <ul className={styles["experience-list"]}>
+              {experiencesWithActive.map((experience) => (
+                <ExperienceCard
+                  key={experience.title}
+                  title={experience.title}
+                  company={experience.company}
+                  startDate={experience.startDate}
+                  endDate={experience.endDate}
+                  active={experience.active}
+                  onClick={() => handleActiveExperienceChange(experience.title)}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </section>
